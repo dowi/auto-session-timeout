5ca725dfe9f516f50541ae729b72439fb7e9a5d7
Dovi Winberger <dowi84@gmail.com>
2017-02-14 16:27:51 +0200

    fixed new line typo

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -4,13 +4,12 @@ module AutoSessionTimeoutHelper
     verbosity = options[:verbosity] || 2
     run_at_once = options[:run_at_once] == 0 ? false : true
     code = <<JS
-
     $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:#{verbosity}, runatonce:#{run_at_once}}, function(remoteData, success) {
       if (success == 'success' && remoteData == 'false')
         window.location.href = '/timeout';
     });
-
     JS
+
     javascript_tag(code)
   end
 end
