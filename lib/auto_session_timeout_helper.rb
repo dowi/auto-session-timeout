f304710f397df4f655156aa81b6fe16bc480754b
Dovi Winberger <dowi84@gmail.com>
2017-02-14 16:13:01 +0200

    add run_at_once option +

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -2,31 +2,15 @@ module AutoSessionTimeoutHelper
   def auto_session_timeout_js(options={})
     frequency = options[:frequency] || 60
     verbosity = options[:verbosity] || 2
+    run_at_once = options[:run_at_once] == 0 ? false : true
     code = <<JS
-if (typeof(Ajax) != 'undefined') {
-  new Ajax.PeriodicalUpdater('', '/active', {frequency:#{frequency}, method:'get', onSuccess: function(e) {
-    if (e.responseText == 'false') window.location.href = '/timeout';
-  }});
-}else if(typeof(jQuery) != 'undefined'){
-  function PeriodicalQuery() {
-    $.ajax({
-      url: '/active',
-      success: function(data) {
-        if(data == 'false'){
-          window.location.href = '/timeout';
-        }
-      }
+
+    $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:#{verbosity}, runatonce:#{run_at_once}}, function(remoteData, success) {
+      if (success == 'success' && remoteData == 'false')
+        window.location.href = '/timeout';
     });
-    setTimeout(PeriodicalQuery, (#{frequency} * 1000));
-  }
-  setTimeout(PeriodicalQuery, (#{frequency} * 1000));
-} else {
-  $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:#{verbosity}}, function(remoteData, success) {
-    if (success == 'success' && remoteData == 'false')
-      window.location.href = '/timeout';
-  });
-}
-JS
+
+    JS
     javascript_tag(code)
   end
 end
